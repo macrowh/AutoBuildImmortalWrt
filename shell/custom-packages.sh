32143c550fb06c1c1121af2621cf0ede2d1cce90
macrowh <macrowh@gmail.com>
2025-12-27 12:17:48 +0800

    Update custom-packages.sh
    保留mosdns,zerotier

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -22,11 +22,11 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-nekobox"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES momo luci-app-momo luci-i18n-momo-zh-cn"
 # 同样是代理相关 但以下2个属于imm仓库内的软件 一般在build24.sh中已经集成 你也可以在此处调整它的去留 若去除组件则使用减号- 若添加则 不使用减号 或者 不处理
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
 # zerotier
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES zerotier luci-app-zerotier"
 # SmartDNS 服务器
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES smartdns luci-app-smartdns"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES smartdns luci-app-smartdns"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-homeproxy-zh-cn"
 # VPN
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale luci-i18n-tailscale-zh-cn"
@@ -40,7 +40,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES smartdns luci-app-smartdns"
 # 网络测速 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
 # MosDNS
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
 # Turbo ACC 网络加速
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
 # 应用过滤 openappfilter.com
